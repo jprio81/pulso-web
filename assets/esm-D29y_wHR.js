b8454326c649d8f32e58aa1b47cb2567a2196f98
Juan Pablo Rio <jprio@alternea.eu>
2026-07-16 21:21:30 +0200

    deploy 2026-07-16 21:21

--- assets/esm-D29y_wHR.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BJEYQxXc.js","assets/index-C9PTO-fo.js","assets/index-CS9vEs6R.css"])))=>i.map(i=>d[i]);
-import{n as e,r as t}from"./index-C9PTO-fo.js";var n=e(`App`,{web:()=>t(()=>import(`./web-BJEYQxXc.js`).then(e=>new e.AppWeb),__vite__mapDeps([0,1,2]))});export{n as App};
