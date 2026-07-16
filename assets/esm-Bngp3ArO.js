2d8885f62bb0e87bc398c5eb28d88aca80db1ce2
Juan Pablo Rio <jprio@alternea.eu>
2026-07-16 21:04:44 +0200

    deploy 2026-07-16 21:04

--- assets/esm-Bngp3ArO.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web--rZCfN9X.js","assets/index-BtMZH-K7.js","assets/index-CBSxNcIa.css"])))=>i.map(i=>d[i]);
-import{n as e,r as t}from"./index-BtMZH-K7.js";var n=e(`App`,{web:()=>t(()=>import(`./web--rZCfN9X.js`).then(e=>new e.AppWeb),__vite__mapDeps([0,1,2]))});export{n as App};
