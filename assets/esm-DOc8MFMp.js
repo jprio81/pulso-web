de931870a3b1ddd22b3593f986b2af6bff6d7827
Juan Pablo Rio <jprio@alternea.eu>
2026-07-16 20:09:01 +0200

    deploy 2026-07-16 20:09

---
+++ assets/esm-DOc8MFMp.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bx74Ysk8.js","assets/index-nfTB1Q9B.js","assets/index-B4noldWB.css"])))=>i.map(i=>d[i]);
+import{n as e,r as t}from"./index-nfTB1Q9B.js";var n=e(`App`,{web:()=>t(()=>import(`./web-Bx74Ysk8.js`).then(e=>new e.AppWeb),__vite__mapDeps([0,1,2]))});export{n as App};
