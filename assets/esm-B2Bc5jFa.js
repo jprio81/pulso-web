e2fb67ee5f4ce480108adff89abfad4045837d33
Juan Pablo Rio <jprio@alternea.eu>
2026-07-16 17:07:46 +0200

    deploy 2026-07-16 17:07

---
+++ assets/esm-B2Bc5jFa.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-ByyN7O8U.js","assets/index-DqSNRWHK.js","assets/index-B4noldWB.css"])))=>i.map(i=>d[i]);
+import{n as e,r as t}from"./index-DqSNRWHK.js";var n=e(`App`,{web:()=>t(()=>import(`./web-ByyN7O8U.js`).then(e=>new e.AppWeb),__vite__mapDeps([0,1,2]))});export{n as App};
--- assets/esm-Cm5lKrkz.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-XAtTdwU9.js","assets/index-CWsJ49fd.js","assets/index-B4noldWB.css"])))=>i.map(i=>d[i]);
-import{n as e,r as t}from"./index-CWsJ49fd.js";var n=e(`App`,{web:()=>t(()=>import(`./web-XAtTdwU9.js`).then(e=>new e.AppWeb),__vite__mapDeps([0,1,2]))});export{n as App};
